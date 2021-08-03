d9969ed5a157ff6f2a2b4e6a1cb593fd038d5b1c
Dung Dung <mydung2337@gmail.com>
2021-08-03 10:06:09 +0700

    Added my project

---
+++ generate-data.js
@@ -0,0 +1,36 @@
+const { fake } = require("faker");
+const faker = require("faker");
+const fs = require("fs");
+
+faker.locale = "en";
+
+const randomTodoList = (n) => {
+  if (n <= 0) return [];
+  const totoList = [];
+  Array.from(new Array(n)).forEach(() => {
+    const todo = {
+      id: faker.random.uuid(),
+      name: faker.lorem.words(),
+      completed: faker.random.boolean(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    totoList.push(todo);
+  });
+  return totoList;
+};
+
+(() => {
+  // random data
+
+  const todoList = randomTodoList(15);
+
+  const db = {
+    todo: todoList,
+  };
+
+  // write db object to bd.json
+  fs.writeFile("db.json", JSON.stringify(db), () => {
+    console.log("Generate data successfully");
+  });
+})();
