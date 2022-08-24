ceb114fc5fcee0a8d05f4c2d6684fb3f17806a9e
Dung Phan <mydung2337@gmail.com>
2022-08-24 22:36:34 +0700

    Remove auth

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
+// const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -18,7 +18,7 @@ server.get("/echo", (req, res) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
-server.use(auth);
+// server.use(auth);
 server.use((req, res, next) => {
   if (req.method === "POST") {
     req.body.createdAt = Date.now();
